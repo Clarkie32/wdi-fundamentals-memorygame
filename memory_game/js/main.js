4f890af5e38f0e3e4edae227eb8b0a51dbf36655
Rob Clark <RobClark@Robs-MacBook-Pro.local>
2018-09-20 07:57:32 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["Queen", "Queen", "King", "King"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 
 var cardsInPlay = [];
 
@@ -12,8 +33,10 @@ alert("Sorry, try again.");
 }
 
 var flipCard = function(cardId){
-  console.log("User flipped "+cards[cardId]+"");
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped "+cards[cardId].rank+"");
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
   checkForMatch();
 };
